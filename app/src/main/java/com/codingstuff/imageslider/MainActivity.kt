af5aa17e35986c13f039544d5822370eadba8ed0
wujuichen <stu9930603@gmail.com>
2023-06-20 14:19:59 +0800

    first commit

---
+++ app/src/main/java/com/codingstuff/imageslider/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.codingstuff.imageslider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+import kotlin.math.abs
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var handler : Handler
+    private lateinit var imageList:ArrayList<Int>
+    private lateinit var adapter: ImageAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+        setUpTransformer()
+
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+               super.onPageSelected(position)
+//                handler.removeCallbacks(runnable)
+//                handler.postDelayed(runnable , 2000)
+        //第29和第30行設定頁面自行滑動(可以不用寫)
+            }
+        })
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        handler.removeCallbacks(runnable)
+    }
+
+//   override fun onResume() {
+//        super.onResume()
+//
+//        handler.postDelayed(runnable , 2000)
+//    }
+    //第42到46行是一開始不會執行第一張跳第二張(可以不用寫)
+
+    private val runnable = Runnable {
+        viewPager2.currentItem = viewPager2.currentItem + 1
+    }
+
+    private fun setUpTransformer(){
+        val transformer = CompositePageTransformer()
+        transformer.addTransformer(MarginPageTransformer(30))
+        transformer.addTransformer { page, position ->
+            val r = 1 - abs(position)
+            page.scaleY = 0.85f + r * 0.14f
+        }
+
+        viewPager2.setPageTransformer(transformer)
+    }
+
+    private fun init(){
+        viewPager2 = findViewById(R.id.viewPager2)
+        handler = Handler(Looper.myLooper()!!)
+        imageList = ArrayList()
+
+        //imageList.add(R.layout.page1)
+        //imageList.add(R.layout.page2)
+        //imageList.add(R.layout.page3)
+
+        imageList.add(R.drawable.numberone)
+        imageList.add(R.drawable.numbertwo)
+        imageList.add(R.drawable.numberthree)
+
+        adapter = ImageAdapter(imageList, viewPager2)
+
+        viewPager2.adapter = adapter
+        viewPager2.offscreenPageLimit = 3
+        viewPager2.clipToPadding = false
+        viewPager2.clipChildren = false
+        viewPager2.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+
+    }
+}
